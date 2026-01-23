a59b441b6ff20cf7a02df0a3214469aa6f9d5163
joboy05 <jolidonhoungue30@gmail.com>
2026-01-23 13:37:15 +0100

    fix localhost error

--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const reportController = require('../controllers/reportController');
 const { authenticate } = require('../middleware/auth');
-const { isAdmin } = require('../middleware/rbac'); // Ou roleAuth selon votre structure
+const { requireAdmin } = require('../middleware/rbac'); // Ou roleAuth selon votre structure
 
 // Routes protégées Admin
 router.use(authenticate);
-router.use(isAdmin); // Assurez-vous que ce middleware existe et est importé correctement
+router.use(requireAdmin); // Assurez-vous que ce middleware existe et est importé correctement
 
 router.get('/members', reportController.generateMembersCSV);
 
